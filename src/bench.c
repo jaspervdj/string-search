8a43d93b4da68eda1b3854bf3539bda5e7605f35
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-06 18:45:40 +0200

    Add C benchmark code

---
+++ src/bench.c
@@ -0,0 +1,29 @@
+#include "search.h"
+#include <sys/time.h>
+
+double get_time() {
+    struct timeval t;
+    struct timezone tzp;
+    gettimeofday(&t, &tzp);
+    return t.tv_sec + t.tv_usec*1e-6;
+}
+
+int main(int argc, char **argv) {
+    double mean = 0.0;
+    int runs = 100;
+    int i;
+
+    for(i = 0; i < runs; i++) {
+        double start, stop;
+
+        start = get_time();
+        search(argc, argv);
+        stop = get_time();
+
+        mean += (stop - start) / (double) runs;
+    }
+
+    fprintf(stderr, "Mean: %f\n", mean);
+
+    return 0;
+}
