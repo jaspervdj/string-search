eee16fd450d1aad47e1513c278a3e4938ab8a227
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-07 00:36:04 +0200

    Remove globals in search0

--- src/search0.c
+++ src/search0.c
@@ -4,15 +4,15 @@
 
 #include "common.h"
 
-struct search_data *create_search_data(const char *pattern, int pattern_size) {
+create_search_data(const char *pattern, int pattern_size) {
     return 0;
 }
 
 /**
  * Search a given pattern in a given buffer.
  */
-void search_buffer(struct search_data *data, const char *pattern,
-        int pattern_size, const char *file_name, char *buffer,
+void search_buffer(const char *pattern, int pattern_size,
+        const char *file_name, char *buffer,
         ullong buffer_offset, int buffer_size) {
     int i, j;
 
@@ -26,5 +26,5 @@ void search_buffer(struct search_data *data, const char *pattern,
     }
 }
 
-void free_search_data(struct search_data *data) {
+void free_search_data() {
 }
