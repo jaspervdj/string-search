a062dba1d7152fd8e8803e2016cdafa0259b747e
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-20 00:04:36 +0200

    Abstract pipe diff for N pipes

--- util/pipe-diff.hs
+++ util/pipe-diff.hs
@@ -3,27 +3,35 @@
 module Main where
 
 import System.Environment (getArgs)
-import Control.Concurrent (forkIO)
+import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
 import System.IO (Handle, hGetLine, openFile, IOMode (ReadMode))
-import Control.Monad (forever)
+import Control.Monad (forever, when, forM, forM_, replicateM)
+import Data.List (nub)
 
 producer :: Handle -> Chan String -> IO ()
 producer handle chan = forever $ do
     line <- hGetLine handle
     writeChan chan line
+    threadDelay 1
 
-consumer :: Chan String -> Chan String -> IO ()
-consumer chan1 chan2 = forever $ do
-    l1 <- readChan chan1
-    l2 <- readChan chan2
-    putStrLn $ l1 ++ " ?= " ++ l2
+consumer :: [Chan String] -> Integer -> IO ()
+consumer chans = go
+  where
+    go n = do
+        heads <- forM chans readChan
+        -- when (l1 /= l2) $ putStrLn $ l1 ++ " /= " ++ l2
+        when (n `mod` 10 == 0) $
+            putStrLn $ show n ++ " comparisons done."
+        when (fault heads) $
+            putStrLn $ "Failed: " ++ show heads
+        go (n + 1)
+    fault (x : y : xs) = x /= y || fault (y : xs)
+    fault _  = False
 
 main :: IO ()
 main = do
-    [file1, file2] <- mapM (flip openFile ReadMode) =<< getArgs
-    chan1 <- newChan
-    chan2 <- newChan
-    _ <- forkIO $ producer file1 chan1
-    _ <- forkIO $ producer file2 chan2
-    consumer chan1 chan2
+    handles <- mapM (flip openFile ReadMode) =<< getArgs
+    chans <- replicateM (length handles) newChan
+    forM_ (zip handles chans) $ forkIO . uncurry producer
+    consumer chans 0
