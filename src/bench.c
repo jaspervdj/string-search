328903daca0a6dcbfbbbadbb42151c1db0a18a97
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-20 15:33:16 +0200

    Respect flags in benchmarking code

--- src/bench.c
+++ src/bench.c
@@ -27,6 +27,7 @@ void print_usage(int argc, char **argv) {
     printf("-h         Print this info\n");
     printf("-n num     Number of timed runs (Default: 100)\n");
     printf("-o file    Write benchmarks results to (Default: stderr)\n");
+    printf("-q         Be quiet (Default: no)\n");
     printf("-w num     Number of warmup runs (Default: 10)\n");
     printf("\n");
 }
@@ -37,13 +38,14 @@ void print_usage(int argc, char **argv) {
 int main(int argc, char **argv) {
     int runs = 100;
     FILE *out = stderr;
+    int quiet = 0;
     int warmup_runs = 10;
     int opt_code;
 
     double mean = 0.0;
     int i;
 
-    while((opt_code = getopt(argc, argv, "hn:o:w:")) != -1) {
+    while((opt_code = getopt(argc, argv, "hn:o:qw:")) != -1) {
         switch(opt_code) {
             case 'h':
                 print_usage(argc, argv);
@@ -54,6 +56,9 @@ int main(int argc, char **argv) {
             case 'o':
                 out = fopen(optarg, "w");
                 break;
+            case 'q':
+                quiet = 1;
+                break;
             case 'w':
                 warmup_runs = atoi(optarg);
                 break;
@@ -62,19 +67,29 @@ int main(int argc, char **argv) {
         }
     }
 
-    for(i = 0; i < runs; i++) {
-        double start, stop;
+    /* Warmup runs */
+    for(i = 0; i < warmup_runs; i++) {
+        if(!quiet) fprintf(out, "Warmup %2d/%d...\n", i + 1, warmup_runs);
+        search(argv[optind], argv + optind + 1, argc - optind - 1);
+    }
 
-        fprintf(stderr, "Starting run %2d/%d...\n", i, runs);
+    /* Timed runs */
+    for(i = 0; i < runs; i++) {
+        double run_time, start, stop;
+
+        if(!quiet) fprintf(out, "Run %2d/%d...\n", i + 1, runs);
 
         start = get_time();
         search(argv[optind], argv + optind + 1, argc - optind - 1);
         stop = get_time();
+        run_time = stop - start;
 
-        mean += (stop - start) / (double) runs;
+        if(!quiet) fprintf(out, "Time: %f\n", run_time);
+        mean += run_time / (double) runs;
     }
-
-    fprintf(stderr, "Mean: %f\n", mean);
+    
+    /* Result */
+    fprintf(out, "Mean: %f\n", mean);
 
     return 0;
 }
