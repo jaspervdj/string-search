031d053cf1d24862cb5b503a96087e90f1b947fc
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-10-20 14:35:37 +0200

    Remove superfluous versions

--- src/search1.c
+++ src/search1.c
@@ -3,35 +3,61 @@
 #include <string.h>
 #include <limits.h>
 #include "common.h"
-#include "search1-ullong.h"
-#include "search1-bit-vector.h"
 
-int use_ullong;
+ullong *character_columns;
+ullong match_column;
 
-search_create(const char *pattern, int pattern_size) {
-    use_ullong = 0; pattern_size <= ULLONG_BITS;
-    if(use_ullong) search_create_ullong(pattern, pattern_size);
-    else search_create_brute_force(pattern, pattern_size);
+void search_create(const char *pattern, int pattern_size) {
+    int i;
 
-#   ifdef DEBUG
-    if(use_ullong) printf("Using a single ullong for shift-and.\n");
-    else printf("Using ullong + brute-force for shift-and.\n");
-#   endif
+    /* Allocate room for the character columns. Set all bits to 1. */
+    character_columns = malloc(0x100 * sizeof(ullong));
+    memset(character_columns, 0xff, 0x100 * sizeof(ullong));
+
+    /* Set the correct patterns bits to 0. */
+    for(i = 0; i < MIN(pattern_size, ULLONG_BITS); i++) {
+        character_columns[pattern[i]] &= ~ULLONG_BIT_AT(i);        
+    }
+
+    /* This column is a mask for a match. */
+    match_column = ULLONG_BIT_AT(MIN(pattern_size, ULLONG_BITS) - 1);
 }
 
+/**
+ * Search a given pattern in a given buffer.
+ */
 void search_buffer(const char *pattern,
         int pattern_size, const char *file_name, char *buffer,
         int buffer_size, ullong buffer_offset) {
-    if(use_ullong) {
-        search_buffer_ullong(pattern, pattern_size, file_name, buffer,
-                buffer_size, buffer_offset);
-    } else {
-        search_buffer_brute_force(pattern, pattern_size, file_name, buffer,
-                buffer_size, buffer_offset);
+    ullong column;    
+    const char *buffer_start = buffer;
+    const char *buffer_end = buffer + buffer_size;
+
+    /* We want a buffer, really */
+    if(buffer_size <= 0 || pattern_size <= 0) return;
+
+    /* Initialize the first column */
+    column = (buffer[0] == pattern[0]) ? ~1 : ~0;
+
+    while(buffer < buffer_end) {
+        /* Shift-Or */
+        column = (column << 1) | character_columns[(unsigned char) *buffer];
+
+        /* Check result */
+        if(!(column & match_column)) {
+            /* Further brute-force matching */
+            if(pattern_size <= ULLONG_BITS ||
+                    !memcmp(buffer + 1, pattern + ULLONG_BITS,
+                        pattern_size - ULLONG_BITS)) {
+                print_match(file_name, buffer_offset + (buffer - buffer_start) -
+                        MIN(pattern_size, ULLONG_BITS) + 1);
+            }
+        }
+
+        buffer++;
     }
 }
 
 void search_free() {
-    if(use_ullong) search_free_ullong();
-    else search_free_brute_force();
+    free(character_columns);
 }
