5a12e15e513a3ea21abedcc0e43364d9f05bcf8c
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-11-11 18:40:16 +0100

    Update sources

--- src/bench.c
+++ src/bench.c
@@ -15,7 +15,7 @@ double get_time() {
     struct timeval t;
     struct timezone tzp;
     gettimeofday(&t, &tzp);
-    return t.tv_sec + t.tv_usec*1e-6;
+    return t.tv_sec + t.tv_usec * 1e-6;
 }
 
 /**
