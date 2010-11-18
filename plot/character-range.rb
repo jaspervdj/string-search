404a9721334a09343fe4ea359238cd7f41094dfe
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-11-18 12:11:53 +0100

    Fix character range benchmark

--- plot/character-range.rb
+++ plot/character-range.rb
@@ -9,14 +9,14 @@ def character_range n
     chars.join
 end
 
-range_sizes = (2 .. 256)
+range_sizes = (2 .. 200)
 benchmark range_sizes do |range_size|
     File.open "tmp/characters", "w" do |file|
         file.write(character_range range_size)
     end
 
-    `./util/generate-text -c tmp/characters -s 1 -o tmp/query`
-    `./util/generate-text -c tmp/characters -s 300 -o tmp/text`
+    `./util/generate-text -f tmp/characters -s 1 -o tmp/query`
+    `./util/generate-text -f tmp/characters -s 300 -o tmp/text`
 
     mean = bench "bin/bench2", "tmp/query", "tmp/text"
     [mean]
