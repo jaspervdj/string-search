7cf4cf361e820c1367ff67a6bc11fb5d0fb6c9d3
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-11-19 14:42:29 +0100

    Rabin-Karp improvements

--- src/search3.c
+++ src/search3.c
@@ -3,19 +3,23 @@
 #include <string.h>
 #include "common.h"
 
+int pattern_hash;
+int most_significant_factor;
+
 int hash_code(const char *string, int string_size) {
     int hash = 0;
     int i;
 
     for(i = 0; i < string_size; i++) {
         hash = hash * 2 + ((unsigned char) string[i]);
-        // printf("%d\n", hash);
     }
 
     return hash;
 }
 
 void search_create(const char *pattern, int pattern_size) {
+    pattern_hash = hash_code(pattern, pattern_size);
+    most_significant_factor = 2 << (pattern_size - 2);
 }
 
 /**
@@ -24,19 +28,31 @@ void search_create(const char *pattern, int pattern_size) {
 void search_buffer(const char *pattern, int pattern_size,
         const char *file_name, char *buffer, int buffer_size,
         ullong buffer_offset) {
-    int pattern_hash = hash_code(pattern, pattern_size);
-    int buffer_hash = hash_code(buffer, pattern_size - 1);
-    int i;
+    const char *buffer_start = buffer;
+    /* Where to stop matching */
+    const char *buffer_end = buffer + buffer_size - pattern_size;
+    int buffer_hash = hash_code(buffer, pattern_size);
 
-    for(i = 0; i + pattern_size - 1 < buffer_size; i++) {
-        buffer_hash = (buffer_hash << 1) +
-                (unsigned char) buffer[i + pattern_size - 1];
-
-        if(buffer_hash == pattern_hash) {
-            print_match(file_name, buffer_offset + i);
+    /* Loop through the buffer... */
+    while(buffer < buffer_end) {
+        /* Compare hashes, and strings if necessary */
+        if(buffer_hash == pattern_hash &&
+                !strncmp(buffer, pattern, pattern_size)) {
+            print_match(file_name, buffer_offset + (buffer - buffer_start));
         }
 
-        buffer_hash -= ((unsigned char) buffer[i]) * (2 << (pattern_size - 2));
+        /* Update hash */
+        buffer_hash = ((buffer_hash -
+                ((unsigned char) *buffer) * most_significant_factor) << 1) +
+                (unsigned char) *(buffer + pattern_size);
+
+        buffer++;
+    }
+
+    /* Comparison for the last position */
+    if(buffer_hash == pattern_hash &&
+            !strncmp(buffer, pattern, pattern_size)) {
+        print_match(file_name, buffer_offset + (buffer - buffer_start));
     }
 }
 
